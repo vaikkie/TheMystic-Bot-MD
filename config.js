b05afc2d52a96ca4d773167782b663acc3cdadfa
vaikkie <156680967+vaikkie@users.noreply.github.com>
2024-01-17 23:27:40 -0300

    Update config.js

--- config.js
+++ config.js
@@ -39,8 +39,8 @@ global.owner = [
 global.suittag = ['5219993404349'];
 global.prems = ['51995386439'];
 
-global.packname = 'Sticker';
-global.author = 'The Mystic - Bot';
+global.packname = '© 🇸🇦 EVAN : 米米';
+global.author = '+56961633927';
 global.wm = 'The Mystic - Bot';
 global.titulowm = 'The Mystic - Bot';
 global.titulowm2 = `The Mystic - Bot`
